1b7ab712889020cc1b8952d40830226659b6614c
Dan <dan1ate2@gmail.com>
2015-10-29 12:14:50 +1000

    Added pics for portfolio

--- js/portfolio_grid.js
+++ js/portfolio_grid.js
@@ -4,40 +4,70 @@ $(function(){
         'items' :
         [
             {
-                'title'         : 'Title #1',
-                'description'   : ' Description text here',
-                'thumbnail'     : ['images/small/1.jpg', 'images/small/2.jpg', 'images/small/4.jpg', 'images/small/5.jpg'],
-                'large'         : ['images/large/1.jpg', 'images/large/2.jpg', 'images/large/4.jpg', 'images/large/5.jpg'],
+                'title'         : 'A.C.E.R.S.',
+                'description'   : ' Responsive website using skeleton framework, customized by myself.  This site uses jQuery/JavaScript for its menu and image slider.  Responsive image gallery manually added to this design (jQuery).',
+                'thumbnail'     : ['images/small/acers1.jpg', 'images/small/acers2.jpg'],
+                'large'         : ['images/large/acers1.jpg', 'images/large/acers2.jpg'],
                 'button_list'   :
                 [
-                    { 'title':'Demo', 'url' : 'http://bonchen.net/' },
-                    { 'title':'Download', 'url':'http://porfolio.bonchen.net/'}
+                    { 'title':'Website', 'url' : 'http://www.allcarengines.com.au' }
                 ],
-                'tags'          : ['Portrait']
+                'tags'          : ['Web']
             },
             {
-                'title'         : 'Title #2',
-                'description'   : 'Description text here',
-                'thumbnail'     : ['images/small/4.jpg', 'images/small/5.jpg'],
-                'large'         : ['images/large/4.jpg', 'images/large/5.jpg'],
+                'title'         : 'Creative Intersection',
+                'description'   : ' Creative Intersection blog, customized inside Blogger using their internal CMS and tags.  Used the design style of main website and incorporated into blog design.  Added social media buttons (Facebook) and set up FeedBurner (RSS feed management).',
+                'thumbnail'     : ['images/small/ci1.jpg', 'images/small/ci2.jpg'],
+                'large'         : ['images/large/ci1.jpg', 'images/large/ci2.jpg'],
                 'button_list'   :
                 [
-                    { 'title':'Demo', 'url' : 'http://bonchen.net/' },
-                    { 'title':'Download', 'url':'http://porfolio.bonchen.net/'}
+                    { 'title':'Website', 'url' : 'http://blog.creativeintersection.com.au' }
                 ],
-                'tags'          : ['Vintage']
+                'tags'          : ['Web']
             },
             {
-                'title'         : 'Title #3',
-                'description'   : 'Description text here',
-                'thumbnail'     : ['images/small/15.jpg', 'images/small/2.jpg'],
-                'large'         : ['images/large/15.jpg', 'images/large/2.jpg'],
+                'title'         : 'Hoo Ha Bar',
+                'description'   : 'Located in Southbank, I designed cool posters to bring people into their venue!  These designs are generally monthly, and are my most recent work (2015).',
+                'thumbnail'     : ['images/small/hooha1.jpg', 'images/small/hooha2.jpg', 'images/small/hooha3.jpg', 'images/small/hooha4.jpg', 'images/small/hooha5.png', 'images/small/hooha6.jpg'],
+                'large'         : ['images/large/hooha1.jpg', 'images/large/hooha2.jpg', 'images/large/hooha3.jpg', 'images/large/hooha4.jpg', 'images/large/hooha5.png', 'images/large/hooha6.jpg'],
                 'button_list'   :
                 [
-                    { 'title':'Demo', 'url' : 'http://bonchen.net/' },
-                    { 'title':'Download', 'url':'http://porfolio.bonchen.net/'}
+                    { 'title':'Website', 'url' : 'http://www.hoohabar.com' }
                 ],
-                'tags'          : ['BW']
+                'tags'          : ['Graphic Design']
+            },
+            {
+                'title'         : 'Sky Room',
+                'description'   : 'A simple design for Sky Room in 2012.  Their logo has been retraced and inserted into this piece, the skyline included. <br><i>Now known as Sky Bar (new ownership).</i>',
+                'thumbnail'     : ['images/small/skyroom1.png'],
+                'large'         : ['images/large/skyroom1.png'],
+                'button_list'   :
+                [
+                    { 'title':'Website', 'url' : 'http://www.skyandlotus.com.au' }
+                ],
+                'tags'          : ['Graphic Design']
+            },
+            {
+                'title'         : 'Whygo',
+                'description'   : 'A video conferencing broker, my main client from 2010-2011 working for Creative Intersection.  From organic SEO to Google Adwords for SEM, these guys had a lot to cover as an international organisation targeting 3 countries: Australia, U.K. and U.S., and expanding into Asia.  <br><br>Lots of first page results in Google were gained here! <br><br>Try searching <i>‘video conferencing rooms’</i> in Google and see what comes up!',
+                'thumbnail'     : ['images/small/whygo1.jpg', 'images/small/whygo2.jpg'],
+                'large'         : ['images/large/whygo1.jpg', 'images/large/whygo2.jpg'],
+                'button_list'   :
+                [
+                    { 'title':'Website', 'url' : 'http://www.whygo.net' }
+                ],
+                'tags'          : ['SEO/SEM']
+            },
+            {
+                'title'         : 'Foodtec',
+                'description'   : 'A dairy ingredient supplier in Darra QLD.  Targeting specific dairy related keywords and getting some valuable links to the website along with some website tweaking and directory listings, I was able to bring these guys from the second and third page results to the first page in Google!  <br><br>Try searching <i>‘dairy ingredient suppliers’</i> in Google and see what comes up!',
+                'thumbnail'     : ['images/small/foodtec1.jpg', 'images/small/foodtec2.jpg'],
+                'large'         : ['images/large/foodtec1.jpg', 'images/large/foodtec2.jpg'],
+                'button_list'   :
+                [
+                    { 'title':'Website', 'url' : 'http://www.foodtec.com.au' }
+                ],
+                'tags'          : ['SEO/SEM']
             }
         ]
     });
